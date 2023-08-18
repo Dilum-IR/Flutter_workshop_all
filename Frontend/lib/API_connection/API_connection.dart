e269e2d86a346f4e7eb7ef45949bbfc6c05497e9
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-08-18 18:14:35 +0530

    remove extra

--- Frontend/lib/API_connection/API_connection.dart
+++ Frontend/lib/API_connection/API_connection.dart
@@ -1,7 +1,7 @@
 class API {
   // ipconfig
   // static const host = "https://easelmagazine.lk/travelApp";
-  static const host = "http://10.22.162.94/Flutter_workshop_all/Backend/travelApp";
+  static const host = "http://10.22.162.94/Flutter_workshop_all/Backend/";
   // my.register.lk/clientarea.php/162.214.77.81
   // 172.20.10.10
   static const user = "$host/users";
